dc41002b66761b1216ae6a9249d3896a566c1184
BryanChanona <233299@ids.upchiapas.edu.mx>
2024-11-30 17:12:46 -0600

    Quitando componentes innecesarios de app.components

--- pokedex/src/app/app.component.ts
+++ pokedex/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/header/header.component";
-import { PokemonsComponent } from "./pages/pokemons/pokemons.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, PokemonsComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
